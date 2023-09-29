1bd6efacab716a1a95991a97b02aed209352056f
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-09-29 23:23:41 +0400

    refactor: reduce cognitive complexity in ValidateParamsExist

--- internal/pkg/output/validate_params_exist.go
+++ internal/pkg/output/validate_params_exist.go
@@ -7,30 +7,44 @@ import (
 )
 
 func ValidateParamsExist(o Output) error {
-	names := make(map[string]struct{}, len(o.Params))
+	existing := make(map[string]struct{}, len(o.Params))
 	for _, p := range o.Params {
-		names[p.Name] = struct{}{}
+		existing[p.Name] = struct{}{}
 	}
 
 	var errs []error
+	errs = append(errs, validateParamsExistsInParams(existing, o.Params)...)
+	errs = append(errs, validateParamsExistsInServices(existing, o.Services)...)
 
-	for _, p := range o.Params {
+	return errors.PrefixedGroup("output.ValidateParamsExist: ", errs...)
+}
+
+func validateParamsExistsInParams(existing map[string]struct{}, params []Param) []error {
+	var errs []error
+
+	for _, p := range params {
 		for _, n := range p.DependsOn {
-			if _, ok := names[n]; !ok {
+			if _, ok := existing[n]; !ok {
 				errs = append(errs, fmt.Errorf("%+q: param %+q does not exist", "%"+p.Name+"%", n))
 			}
 		}
 	}
 
-	for _, s := range o.Services {
+	return errs
+}
+
+func validateParamsExistsInServices(existing map[string]struct{}, services []Service) []error {
+	var errs []error
+
+	for _, s := range services {
 		for _, a := range s.AllArgs() {
 			for _, n := range a.DependsOnParams {
-				if _, ok := names[n]; !ok {
+				if _, ok := existing[n]; !ok {
 					errs = append(errs, fmt.Errorf("%+q: param %+q does not exist", "@"+s.Name, n))
 				}
 			}
 		}
 	}
 
-	return errors.PrefixedGroup("output.ValidateParamsExist: ", errs...)
+	return errs
 }
